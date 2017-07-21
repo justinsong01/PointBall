bdcb6b988269552f8f4505a663d0a5a20816c6a7
Justin Song <yjsong0404@gmail.com>
2017-07-21 13:11:21 -0400

    Move a Ball
    Commited with a final changes + exported packages

---
+++ MovingBall/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveBall : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody Sphere;
+    private int count;
+    public Text countText;
+    public Text winMessage;
+
+	// Use this for initialization
+	void Start () {
+		Sphere = GetComponent<Rigidbody>();
+        count = 0;
+        setCountBall ();
+        winMessage.text = "";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float horizontalPoint = Input.GetAxis ("Horizontal");
+		float verticalPoint = Input.GetAxis ("Vertical");
+		Vector3 movePoint = new Vector3 (horizontalPoint, 1.0f, verticalPoint);
+		speed = 5.3f;
+		Sphere.AddTorque(movePoint * speed * Time.deltaTime);
+		Sphere.AddForce (movePoint * speed);
+        //Debug.Log ("Time" + Time.deltaTime);
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+            GetComponent<Rigidbody>().AddForce(jump);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PointCapsule"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            if (count == 5)
+            {
+                winMessage.text = "You Win!";
+            }
+        }
+        setCountBall ();
+    }
+
+    void setCountBall ()
+    {
+        countText.text = "Point: " + count.ToString();
+
+    }
+}
---
+++ MovingBall/Assets/Scripts/rotateCylinder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCylinder : MonoBehaviour {
+    public Color altColor = Color.black;
+    public Renderer rend;
+    // Use this for initialization
+    void Start () {
+        
+        //Get the renderer of the object so we can access the color
+        rend = GetComponent<Renderer>();
+        //Set the initial color (0f,0f,0f,0f)
+        rend.material.color = altColor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            //Alter the color          
+            altColor.g += 0.1f;
+            //Assign the changed color to the material.
+            rend.material.color = altColor;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Alter the color           
+            altColor.r += 0.1f;
+            //Assign the changed color to the material. 
+            rend.material.color = altColor;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            //Alter the color            
+            altColor.b += 0.1f;
+            //Assign the changed color to the material. 
+            rend.material.color = altColor;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Alter the color          
+            altColor.a += 0.1f;
+            //Assign the changed color to the material. 
+            rend.material.color = altColor;
+        }
+        transform.Rotate(new Vector3(0, 0, 10) * Time.deltaTime);
+    }
+
+}
